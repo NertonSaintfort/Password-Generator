e1a966123fe770e25d953de85c049dba5c27fdbd
NertonSaintfort <127815573+NertonSaintfort@users.noreply.github.com>
2024-10-30 13:50:55 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let generateBtn = document.getElementById("generatebtn");
+let showResults1 = document.getElementById("displaypass1");
+let showResults2 = document.getElementById("displaypass2");
+
+generateBtn.addEventListener("click", function () {
+  function generatePassword() {
+    let showResults1 = "";
+    let showResults2 = "";
+    for (let i = 0; i < 15; i++) {
+      showResults1 += characters[Math.floor(Math.random() * characters.length)];
+      showResults2 += characters[Math.floor(Math.random() * characters.length)];
+    }
+    return showResults1;
+    return showResults2;
+  }
+
+  showResults1.textContent = generatePassword();
+  showResults2.textContent = generatePassword();
+});
